5281a23abb5ad6ea0d586bbd2ed081fdfb1219ec
Khushikaushall <khushikaushal2000@gmail.com>
2026-06-19 19:59:23 +0530

    Day-19 assignment is uploaded

---
+++ Day-19/Q78.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+//program to sum of diagonal of a matrix
+int main() {
+    int r,c;
+    cout<<"Enter value of row and column: ";
+    cin>>r>>c;
+    int A[r][c];
+
+    cout<<"Enter value of matrix A:"<<endl;
+    for(int i=0;i<r;i++) {
+        for(int j=0;j<c;j++) {
+            cin>>A[i][j];
+        }
+    }
+
+    int sum=0,sum1=0;
+    for(int i=0;i<r;i++) {
+        sum+=A[i][i];          // principal diagonal: i==j
+        sum1+=A[i][r-1-i];     // anti diagonal: i+j==r-1
+    }
+
+    cout<<"Sum of principal diagonal: "<<sum<<endl;
+    cout<<"Sum of anti diagonal: "<<sum1<<endl;
+
+    return 0;
+}
